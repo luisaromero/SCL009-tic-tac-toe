dd785e7f2e91edd51f4fb6132179bdf75012e74c
luisaromero <luisa.marcela.romero@gmail.com>
2019-09-04 17:36:58 -0400

    change color of background

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <View style={styles.square}/>
+       
     </View>
   );
 }
@@ -12,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7E57C2',
     alignItems: 'center',
     justifyContent: 'center',
+    top:20
   },
+  square: {
+   left:30,
+    borderLeftWidth:7,
+    height:300,
+    width:300,
+  }
 });
